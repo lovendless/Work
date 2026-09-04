7e1143a54503a9a0bb93b0f01a6467dcd60f16d3
lovendless <129157088+lovendless@users.noreply.github.com>
2026-09-04 19:26:10 +0700

    Add files via upload

---
+++ Кастомные платежные страницы/success.php
@@ -0,0 +1,9 @@
+ 	<meta charset="UTF-8">
+ </head>
+ <body style="background: white !important; font-family: sans-serif;">
+
+ 	<img src="images/success.png" style="width: 200px; display: block; margin: 0 auto; margin-top: 40px;">
+
+ 	<div style="font-weight: bold; text-align: center; font-size: 28px; margin-top: 30px;">Поздравляем!<br>Платеж прошел успешно!</div>
+ 	<div style="text-align: center; margin-top: 15px;">Спасибо за Ваш заказ! Скоро Вам придет SMS с подтверджением оплаты заказа.</div>
+ </body>
